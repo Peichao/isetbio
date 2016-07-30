af14673c91bbb8eeb55a684facd1f86d7725aab4
Nicolas Cottaris <cottaris@sas.upenn.edu>
2016-07-30 23:58:09 +0300

    Unit tests for pdist2smallest
    Signed-off-by: Nicolas Cottaris <cottaris@sas.upenn.edu>

--- external/pdist2/pdist2smallest.m
+++ external/pdist2/pdist2smallest.m
@@ -1,8 +1,9 @@
 function [D, I] = pdist2smallest(X, Y)
 %pdist2smallest Pairwise distance between two sets of observations.
-% D = pdist2smallest(X,Y) returns a [1 x My] matrix D containing the minimum Euclidean 
-% distance between each point in the My-by-N data  matrix Y to all
-% points in the MX-by-N data matrix X.
+% [D, I] = pdist2smallest(X,Y) returns a [1 x My] matrix D containing the minimum Euclidean 
+% distances between each point in the My-by-N data matrix Y to all
+% points in the MX-by-N data matrix X. The indices of the points in the data
+% matrix X, which result in the minimun distances are also returned in the [1 x My] matrix, I.
 %
 %
 % NPC ISETBIO Team, Copyright 2016
---
+++ external/pdist2/t_pdist2smallest.m
@@ -0,0 +1,22 @@
+function t_pdist2smallest
+% Unit tests for pdist2smallest
+%
+% NPC ISETBIO Team, Copyright 2016
+
+    mDims = 2;
+    nXPoints = 10;
+    X = rand(xXpoints,mDims);
+    nYPoints = 3;
+    Y = rand(nYPoints,mDims);
+    
+    directOutput = zeros(1, nYPoints);
+    for pt1 = 1:nYPoints
+        d = sqrt(sum((bsxfun(@minus, X, Y(pt1,:))).^2,2));
+        directOutput(1,pt1) = min(d);
+    end
+    directOutput
+    
+    [matlabOutput, matlabIndices] = pdist2(X, Y, 'euclidean', 'Smallest', 1)
+    
+    [mexOutput, mexIndices] = pdist2smallest(X, Y)
+end
